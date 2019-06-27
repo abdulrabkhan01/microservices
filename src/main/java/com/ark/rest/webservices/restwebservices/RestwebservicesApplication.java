379a17ea70a011e4e2a38970db337668c9565ba6
abdul <abdul>
2019-06-28 01:03:24 +0530

    adding source

---
+++ src/main/java/com/ark/rest/webservices/restwebservices/RestwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.ark.rest.webservices.restwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwebservicesApplication.class, args);
+	}
+
+}
